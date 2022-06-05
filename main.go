eae1d42af12af8ddc8c9c45592d9692bd845eadc
quanscheng <quanscheng@163.com>
2022-06-05 23:45:30 +0800

    命名返回值

--- main.go
+++ main.go
@@ -4,11 +4,22 @@ import (
 	"fmt"
 )
 
+// 多个返回值案例
 func swap(x, y string) (string, string) {
 	return y, x
 }
 
+// 命名返回值案例
+func split(sum int) (x, y int) {
+	x = sum + 2
+	y = sum - x
+	return
+}
+
 func main() {
 	a, b := swap("传参一", "传参二")
+
 	fmt.Println(a, b)
+
+	fmt.Println(split(6))
 }
